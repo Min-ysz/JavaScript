dac7e1fd40f471d9845c5a431cd891ab460e23a3
Yasmim <120032313+Min-ysz@users.noreply.github.com>
2024-06-25 14:34:27 -0300

    Add files via upload

---
+++ GitHub/JavaScript/assets/script.js
@@ -0,0 +1,12 @@
+const num1 = document.getElementById("number1");
+const num2 = document.getElementById("number2");
+const botao = document.getElementById("botao");
+
+botao.addEventListener ("click", function() {
+
+    resultado = number1+number2
+
+    alert("A soma dos números é:" + resultado )
+});
+
+    
